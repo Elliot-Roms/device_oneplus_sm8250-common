3f05255d79bf21e88f0360cb47d645e076855c6f
Jamal <jamal2362@googlemail.com>
2022-01-13 15:26:08 +0100

    Add OnePlus Camera support

--- common.mk
+++ common.mk
@@ -23,6 +23,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/oneplus/sm8250-common/sm8250-common-vendor.mk)
 
+# OnePlus Camera
+$(call inherit-product-if-exists, vendor/oneplus/opcamera/opcamera-vendor.mk)
+
 # Include new Interfaces makefile
 $(call inherit-product, $(LOCAL_PATH)/interfaces.mk)
 
@@ -190,7 +193,8 @@ PRODUCT_PACKAGES_DEBUG += \
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
-    Snap \
+    libcamera2ndk_vendor \
+    vendor.oneplus.hardware.camera@1.0 \
     vendor.qti.hardware.camera.postproc@1.0.vendor
 
 # Common init scripts
@@ -205,6 +209,7 @@ PRODUCT_PACKAGES += \
     init.color.rc \
     init.oem.sec.rc \
     init.oem_ftm.rc \
+    init.opcamera.rc \
     init.oplus_chg.sh \
     init.qcom.class_core.sh \
     init.qcom.coex.sh \
@@ -304,6 +309,9 @@ PRODUCT_PACKAGES += \
 
 # HIDL
 PRODUCT_PACKAGES += \
+    libhidltransport \
+    libhidltransport.vendor \
+    libhwbinder \
     libhwbinder.vendor
 
 # HotwordEnrollement app permissions
