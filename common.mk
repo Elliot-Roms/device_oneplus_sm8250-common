17004117fd885739662affa4d8c102f19560e10b
Jamal <jamal2362@googlemail.com>
2022-01-13 14:58:50 +0100

    Add Interfaces files for incoming OOS camera

--- common.mk
+++ common.mk
@@ -23,6 +23,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/oneplus/sm8250-common/sm8250-common-vendor.mk)
 
+# Include new Interfaces makefile
+$(call inherit-product, $(LOCAL_PATH)/interfaces.mk)
+
 # Additional native libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
@@ -405,8 +408,7 @@ PRODUCT_PACKAGES += \
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH) \
-    device/oneplus/common
+    $(LOCAL_PATH)
 
 # Telephony
 PRODUCT_PACKAGES += \
