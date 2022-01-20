b8bedc1b1c526cc759df185b0301e657b896644e
Jamal <jamal2362@googlemail.com>
2022-01-20 10:59:11 +0100

    sm8250-common: android.hardware.power-service-qti

--- common.mk
+++ common.mk
@@ -381,7 +381,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service \
+    android.hardware.power-service-qti \
     vendor.qti.hardware.perf@2.0.vendor
 
 # QMI
