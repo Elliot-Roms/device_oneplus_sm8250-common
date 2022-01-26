d7012bc4ba07c1dc728d921eeea7a704d4c4f93b
jamal2362 <jamal2362@googlemail.com>
2022-01-26 15:11:45 +0100

    Revert "UDFPS improvements"
    This reverts commit 9110837759183c39e010e45dcb0d97c3904129dd.

--- common.mk
+++ common.mk
@@ -281,6 +281,7 @@ PRODUCT_PACKAGES += \
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.3-service.oneplus_kona \
+    android.hardware.biometrics.fingerprint@2.3.vendor \
     vendor.oneplus.fingerprint.extension@1.0 \
     vendor.oneplus.hardware.display@1.0
 
--- fingerprint/UdfpsExtension.cpp
+++ fingerprint/UdfpsExtension.cpp
@@ -15,6 +15,7 @@
  */
 
 #include <compositionengine/UdfpsExtension.h>
+#include <vendor/oneplus/hardware/display/1.0/IOneplusDisplay.h>
 
 uint32_t getUdfpsZOrder(uint32_t z, bool touched) {
     return touched ? 0xfc8 : z;
